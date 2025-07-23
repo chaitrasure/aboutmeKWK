0d40792e515c39604f3288f2fd64aaec54f2843d
Chaitra Sure <chaitrasure@gmail.com>
2025-07-23 13:08:28 -0700

    Initial Commit

---
+++ aboutmeKWK/aboutmeKWKApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutmeKWKApp.swift
+//  aboutmeKWK
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct aboutmeKWKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
