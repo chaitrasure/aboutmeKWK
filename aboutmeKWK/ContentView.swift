8da8fcc978cff00be4116a40c3bb0597665988a5
Chaitra Sure <chaitrasure@gmail.com>
2025-07-23 14:13:29 -0700

    deleted the ignoring safe spaces

--- aboutmeKWK/ContentView.swift
+++ aboutmeKWK/ContentView.swift
@@ -9,11 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color("pink")
+              
+               
+            VStack(alignment: .center, spacing: 15.0) {
+                
+                Text("Hi! I'm Chaitra")
+                    .font(.system(size: 24))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .underline()
+                
+                Image("Image from iOS")
+                    .resizable()
+                    .frame(width: 320, height: 350)
+                
+                
+                Text("I'm from Los Angeles, and I love art, reading, and watching new TV shows.")
+                    .font(.system(size: 20))
+                   
+                
+            }
         }
         .padding()
     }
